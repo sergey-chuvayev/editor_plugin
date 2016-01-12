4ed6d7ee6a1621826620709c53c2f9abb969a426
Sergey <sergey@lookatme.ru>
2016-01-12 15:17:48 +0300

    comment

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 $(window).keydown(function (e){
-    if ((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode == 83) { /*ctrl+s or command+s*/
+    if ((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode == 83) { /*ctrl+shift+s or command+shift+s*/
         console.info('saving post...');
         $('[role="post:save"]').first().trigger('click');
         e.preventDefault();
